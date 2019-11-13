4261e95c669887d867062d9f3f96135bbd66c13e
Nax <max@bacoux.com>
2019-11-13 23:27:01 +0100

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# picture-loader
-An advanced image loader for webpack
+# image-convert-loader
+
+A webpack loader to preprocess images.
+
+## Install
+
+    yarn add -D image-convert-loader
+
+## Usage
+
+This loader is typically used with other loaders such as file-loader or img-loader.
+
+Exemple conf:
+
+    {
+        test: /\.(png|jpg|webp)$/,
+        use: [
+            'file-loader',
+            'img-loader',
+            'image-convert-loader'
+        ]
+    }
+
+You can then pass query arguments when loading images:
+
+    const url = require('./foo.png?w=512&format=webp');
+
+## Options
+
+The following options are accepted as query arguments:
+
+**w** | **h**: Sets the width and/or the height of the resulting image. If one is missing, the aspect ratio will be preserved.  
+**format**: Sets the format of the resulting image. valid values are `png`, `jpg` and `webp`.
+
+## License
+
+MIT.
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const sharp = require('sharp');
+const loaderUtils = require('loader-utils');
+
+module.exports = function (content, map, meta) {
+  const callback = this.async();
+  const image = sharp(content);
+  const query = this.resourceQuery[0] === '?' ?  loaderUtils.parseQuery(this.resourceQuery) : {};
+  const p = path.parse(this.resourcePath);
+  const that = this;
+
+  image.metadata(function(err, metadata) {
+    if (err) throw err;
+
+    let w = null;
+    let h = null;
+
+    if (query.w) {
+      w = Number(query.w);
+    }
+
+    if (query.h) {
+      h = Number(query.h);
+    }
+
+    if (w || h) {
+      if (!w) {
+        w = Math.round(h / metadata.height);
+      }
+
+      if (!h) {
+        h = Math.round(w / metadata.width);
+      }
+
+      image.resize(w, h);
+      p.name += ('-' + w + 'x' + h);
+    }
+
+    if (query.format) {
+      image.toFormat(query.format);
+      p.ext = '.' + query.format;
+    }
+
+    that.resourcePath = path.format({ dir: p.dir, name: p.name, ext: p.ext });
+
+    image.toBuffer(function (err, data) {
+      if (err) throw err;
+      callback(null, data, map, meta);
+    });
+  });
+};
+module.exports.raw = true;
